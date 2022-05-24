84c2c533bdb5cfcecc0a41e94685be7bb777051d
tdcpinto <theo.de-castro-pinto@u-bordeaux.fr>
2022-05-24 17:40:21 +0200

    Auto-push!

--- latex_cmd.sh
+++ latex_cmd.sh
@@ -2,3 +2,8 @@ function initlatex {
     SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     cp -r "$SCRIPT_DIR/__latex/"* .
 }
+
+function initbeamer {
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    cp -r "$SCRIPT_DIR/__beamer/"* .
+}
